6b2c0e92e7c9b6a8aa1c823faeb7532606edbc27
khai <khaitruong1301@gmail.com>
2026-01-02 18:43:55 +0700

    js

--- Models/DTO/StoreDTO.cs
+++ Models/DTO/StoreDTO.cs
@@ -1,20 +1,27 @@
-   //Entities, ViewModel, DTO , Model
-   public partial class ResponseData
-    {
-        public long statusCode { get; set; }
-        public string message { get; set; }
-        public List<StoreDTO> content { get; set; }
-        public DateTimeOffset dateTime { get; set; }
-    }
+//Entities, ViewModel, DTO , Model
+public partial class ResponseData<T>
+{
+    public long statusCode { get; set; }
+    public string message { get; set; }
+    public T content { get; set; }
+    public DateTimeOffset dateTime { get; set; }
+}
 
-    public partial class StoreDTO
-    {
-        public long id { get; set; }
-        public string name { get; set; }
-        public string alias { get; set; }
-        public string latitude { get; set; }
-        public string longtitude { get; set; }
-        public string description { get; set; }
-        public Uri image { get; set; }
-        public bool deleted { get; set; }
-    }
+public partial class StoreDTO
+{
+    public long id { get; set; }
+    public string name { get; set; }
+    public string alias { get; set; }
+    public string latitude { get; set; }
+    public string longtitude { get; set; }
+    public string description { get; set; }
+    public Uri image { get; set; }
+    public bool deleted { get; set; }
+}
+
+public class StoreDetailDTO : StoreDTO
+{
+
+    
+}
+
